213f21eef75e83e68e5b71ea01d8b529ac5fa715
GosGuru <mporcile59@gmail.com>
2025-09-26 19:21:14 -0300

    bh}

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+
+const r = (...segments: string[]) => path.resolve(path.dirname(fileURLToPath(import.meta.url)), ...segments);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": r("./src"),
+    }
+  }
 });
